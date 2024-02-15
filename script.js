cb72fb3cf7d36c90003225f4232cfcb87d700ae6
hanna522 <145169578+Hangyeol522@users.noreply.github.com>
2024-02-15 15:48:19 -0800

    changed to grid display and added color options

--- script.js
+++ script.js
@@ -1,22 +1,76 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const girdContainer = document.getElementById("grid-container");
+    const gridContainer = document.querySelector(".grid-container");
+    const newGridButton = document.querySelector(".newGridButton");
+    let isDrawing = false;
+    let color = 'black';
 
-    for (let i = 0; i < 16 * 16; i++) {
-        const square = document.createElement('div'); // create a new div element
-        square.classList.add('square'); // add the 'square' class to the div
-        girdContainer.appendChild(square); //append the div to the document body
-        
-        // Hovering effect
-        square.addEventListener('mouseenter', () =>{
-            square.style.backgroundColor = 'pink';
-        })
-        square.addEventListener('mouseleave', () => {
-            square.style.backgroundColor = '';
-        })
-    };
+    createGrid(16);
 
-    const btn = document.getElementById('popup-button');
+    function startDrawing(square) {
+        isDrawing = true;
+        square.style.backgroundColor = color;        
+    }
 
-    btn.addEventListener('onclick')
+    function Draw(square) {
+        if (isDrawing) {
+            square.style.backgroundColor = color;  
+        }
+    }
 
+    function stopDrawing() {
+        isDrawing = false;
+    }
+
+    function createGrid(size) {
+        const squares = gridContainer.querySelectorAll('div');
+        squares.forEach((div) => div.remove());
+        gridContainer.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+        for (let i = 0; i < size * size; i++) {
+            const square = document.createElement('div');
+            square.style.backgroundColor = "white";
+            gridContainer.insertAdjacentElement("beforeend", square);
+
+            square.addEventListener('mousedown', () => {
+                startDrawing(square);
+            });
+
+            square.addEventListener('mouseup', () => {
+                stopDrawing();
+            });
+
+            square.addEventListener('mousemove', () => {
+                Draw(square);
+            });
+        }
+    }
+
+    function changeColor(newColor) {
+        color = newColor;
+    }
+    
+    const colorButtons = document.querySelectorAll('.colors button');
+    colorButtons.forEach((button) => {
+        button.addEventListener('click', function() {
+            changeColor(this.className); 
+        });
+    });
+
+    const eraseButton = document.querySelector('.options .eraser');
+    eraseButton.addEventListener('click', () => {
+        changeColor('white');
+    })
+
+    newGridButton.addEventListener('click', () => {
+        let userInput = document.querySelector('.grid-size');
+        let newSize = parseInt(userInput.value, 10);
+        newSize = Math.min(newSize, 100);
+        if (isNaN(newSize) || newSize <= 0) {
+            alert("Please enter a valid positive number.");
+            return;
+        }
+
+        createGrid(newSize);
+    });
 });
